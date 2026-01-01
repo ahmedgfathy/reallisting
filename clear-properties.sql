4654f3cc149e145abb8e03290e7a050865855f17
“Ahmed <ahmedgfathy@gmail.com>
2026-01-01 13:31:04 +0200

    Organize project structure: move cleaning scripts, database scripts, and docs to proper folders

--- clear-properties.sql
+++
@@ -1,5 +0,0 @@
--- Clear all properties data to restart migration with correct parsing
-DELETE FROM glomar_property_videos;
-DELETE FROM glomar_property_images;
-DELETE FROM glomar_properties;
-DELETE FROM glomar_projects;
